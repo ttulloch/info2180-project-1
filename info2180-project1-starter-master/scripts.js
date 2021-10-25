b6a062c1aa94d8cd167c6a8aaa5a1d3d2eb42912
ttulloch <74498718+ttulloch@users.noreply.github.com>
2021-10-25 07:56:55 -0500

    Add files via upload

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,17 +1,18 @@
 /* Add your JavaScript to this file */
 
 window.onload = function(){
-
-    var btn = document.querySelector("button");
     var message = document.getElementsByClassName("message")[0];
+    var btn = document.querySelector("button");
     var email=document.getElementById("email");
     btn.addEventListener("click", function(i){
         i.preventDefault();
         var email = document.getElementById("email").value;
-        if (document.getElementById("email").value = ""){
-            textMsg.innerHTML = "Successful.Thank you for adding your email address" + email + "has been added to our mailing list!";
-        } else {
+        if (document.getElementById("email").value === ""){
             textMsg.innerHTML = "Please enter a valid email address";
+            
+        } else {
+            textMsg.innerHTML = "Successful.Thank you, your email address:" + email + "has been added to our mailing list!";
+            
         }
 
 
